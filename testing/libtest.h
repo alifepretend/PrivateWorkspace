eba837809ee8ddded01090f62e66deca13d17262
alifepretend <gustavodbem@outlook.com>
2024-08-06 12:00:20 -0300

    a

---
+++ testing/libtest.h
@@ -0,0 +1,7 @@
+#ifndef LIBTEST_H
+#define LIBTEST_H
+
+static int banana = 0;
+
+
+#endif
---
+++ testing/test2.cpp
@@ -0,0 +1,10 @@
+#include "libtest.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << banana << endl;
+	return 1;
+}
