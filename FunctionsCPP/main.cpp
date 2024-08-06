7f50edb3d446860a6b7e6e25c5e353f22f016f5f
alifepretend <gustavodbem@outlook.com>
2024-08-06 10:03:32 -0300

    a

--- FunctionsCPP/main.cpp
+++ FunctionsCPP/main.cpp
@@ -1,16 +1,5 @@
 #include "serialib.h"
 #include <stdio.h>
-
-#if defined (_WIN32) || defined(_WIN64)
-    //for serial ports above "COM9", we must use this extended syntax of "\\.\COMx".
-    //also works for COM0 to COM9.
-    //https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea?redirectedfrom=MSDN#communications-resources
-#define SERIAL_PORT "\\\\.\\COM13"
-#endif
-#if defined (__linux__) || defined(__APPLE__)
-#define SERIAL_PORT "/dev/ttyACM0"
-#endif
-
 #include <iostream>
 #include <vector>
 #include <string> 
@@ -23,7 +12,19 @@
 #include <chrono>
 #include <thread>
 
-#define path "F:/Projects_git/PrivateWorkspace/FunctionsCPP/documents/data1.txt"
+//#if defined (_WIN32) || defined(_WIN64)
+//    //for serial ports above "COM9", we must use this extended syntax of "\\.\COMx".
+//    //also works for COM0 to COM9.
+//    //https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea?redirectedfrom=MSDN#communications-resources
+//#define SERIAL_PORT1 "\\\\.\\COM13"
+//#endif
+//#if defined (__linux__) || defined(__APPLE__)
+//#define SERIAL_PORT1 "/dev/ttyACM0"
+//#endif
+
+
+
+string path = "F:/Projects_git/PrivateWorkspace/FunctionsCPP/documents/data1.txt";
 
 const unsigned long int BAUD_RATE = 115200;
 
@@ -33,9 +34,28 @@ using namespace std::chrono; // nanoseconds, system_clock, seconds
 
 bool device_is_connected = false;
 string line = "";
-IO::Serial serial(SERIAL_PORT, BAUD_RATE);
 
-int main() {
+
+int main(int argc, char* argv[]) {
+	if (argc == 3) {
+		path = argv[2];
+	}
+	else {
+		path = (string)argv[0];
+		while (path[path.size() - 1] != '/') {
+			path.pop_back();
+		}
+	}
+
+	const char* port = "";
+	if (argc > 1) {
+		port = "\\\\.\\" + argv[1][0] + argv[1][1] + argv[1][2] + argv[1][3];
+	} 
+	const char* SERIAL_PORT = (const char*)port;
+
+
+	IO::Serial serial(SERIAL_PORT, BAUD_RATE);
+
 	while (1) {
 		if (serial.erroropening == 1) {
 			int write = serial.receiveData(&line);
@@ -50,9 +70,3 @@ int main() {
 
 	return 0;
 }
-
-// vector<vector<double>> valueTable = IO::parsecsv<double>(IO::readFile(PATH));
-
-//sleep_for(milliseconds(100));
-//sleep_until(system_clock::now() + seconds(1));
-// Continuar daqui
---
+++ Temp_Sense_Arduino/Temp_Sense_STM32.ino
@@ -0,0 +1,81 @@
+#include "pins_arduino.h"
+#include "stm32g4xx_hal_adc.h"
+#include "stm32g4xx_hal_dac.h"
+#include "stm32g4xx_hal_def.h"
+#include <vector>
+
+#define LL_ADC_RESOLUTION LL_ADC_RESOLUTION_12B
+#define ADC_RANGE 4096
+
+using namespace std;
+
+DAC_HandleTypeDef hdac1;
+
+int read;
+// 8 NTC, 1 Pt100
+vector<int> ntc_pins = {A0, A1, A2, A3, A5, A6, A4, D6, D3};
+vector<int> pin_values = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+long int starting_time;
+long int d_time;
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);
+  starting_time = millis();
+
+  delay(1);
+  
+}
+
+void loop() {
+  readNtcAll(ntc_pins, pin_values, d_time);
+
+  delay(20);
+}
+
+static void readNtcAll(vector<int> pins, vector<int> values, long int d_time) {
+  int numberOfPins = pins.size();
+  for(int i = 0; i < numberOfPins; i++) {
+    values[i] = digitalRead(pins[i]);
+  }
+  
+  printValues(values, d_time);
+}
+
+// Formato: "ntc_1","ntc_2","ntc_3","ntc_4","ntc_5","ntc_6","ntc_7","ntc_8","PT100","d_time";
+static void printValues(vector<int> values, long int d_time) {
+  int numberOfValues = values.size();
+
+  for (int i = 0; i < numberOfValues; i++) {
+    Serial.print("\"");
+    Serial.print(values[i]);
+    Serial.print("\",");
+
+    if(i == (numberOfValues - 1)) {
+      Serial.print("\"");
+      d_time = millis() - starting_time;
+      Serial.print(d_time);
+      Serial.println("\";");
+    }
+  }
+}
+
+
+static float readVdd()
+{
+  return (1.20 * 4096.0 / analogRead(ADC_CHANNEL_VREFINT)); // ADC sample to V
+  //return (1200 * 4096 / adc_read(ADC_CHANNEL_VREFINT)); // ADC sample to mV
+}
+
+static int32_t readVref()
+{
+  return (__LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION));
+}
+
+#ifdef ATEMP
+static int readTempSensor(int VRef)
+{
+  return (__LL_ADC_CALC_TEMPERATURE(VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
+}
+#endif
+
