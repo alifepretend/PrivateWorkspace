663c4aaaf2c857c8dcb51ae5d27c34c4f27d1699
alifepretend <gustavodbem@outlook.com>
2024-08-11 09:50:11 -0300

    a

--- FunctionsCPP/IO.h
+++ FunctionsCPP/IO.h
@@ -12,7 +12,6 @@
 #include <codecvt>
 #include <locale>
 
-
 using namespace std;
 
 class IO
--- FunctionsCPP/main.cpp
+++ FunctionsCPP/main.cpp
@@ -4,12 +4,12 @@
 #include <string> 
 #include <array>
 #include <cstring> 
-#include "math.h"
+#include <chrono>
+#include <thread>
 #include <fstream>
 #include <sstream>
 #include "IO.h"
-#include <chrono>
-#include <thread>
+#include "Math.h"
 
 using namespace std;
 using namespace std::this_thread; // sleep_for, sleep_until
@@ -17,14 +17,18 @@ using namespace std::chrono; // nanoseconds, system_clock, seconds
 
 string path = "F:/Projects_git/PrivateWorkspace/FunctionsCPP/approximation.txt";
 
+
 int main() {
 	vector<vector<double>> adc_esp = IO().parsecsv(IO().readFile("F:\\Projects_git\\PrivateWorkspace\\FunctionsCPP\\adc_esp.txt"));
 	vector<vector<double>> tensao_arduino = IO().parsecsv(IO().readFile("F:\\Projects_git\\PrivateWorkspace\\FunctionsCPP\\tensao_arduino.txt"));
+
 	cout << tensao_arduino.size() << endl;
 	cout << adc_esp.size() << endl;
+	
 	vector<vector<double>> approximation = IO().approximateGivenTheTime(&tensao_arduino, adc_esp);
 	cout << approximation.size();
 
+	
 
 	for (int i = 0; i < approximation.size(); i++) {
 		string line = "";
@@ -34,17 +38,6 @@ int main() {
 		line += "\n";
 		cout << line << endl;
 
-
-		//for (int j = 0; j < approximation[i].size(); j++) {
-		//	cout << approximation[i][j];
-		//	line.append(to_string(approximation[i][j]));
-		//	if (j == 0) {
-		//		line.append(",");
-		//	}
-		//	else {
-		//		line.append("\r\n");
-		//	}
-		//}
 
 		IO().writeFile(line, path);
 		
